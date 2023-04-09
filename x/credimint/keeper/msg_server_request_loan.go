d7a9e80e9e6b15a16496144987ae6f18b8c4eaf5
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2023-04-09 16:41:26 +0530

    code: Modify request loan logic

--- x/credimint/keeper/msg_server_request_loan.go
+++ x/credimint/keeper/msg_server_request_loan.go
@@ -6,17 +6,45 @@ import (
 	"credimint/x/credimint/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"strconv"
 )
 
 func (k msgServer) RequestLoan(goCtx context.Context, msg *types.MsgRequestLoan) (*types.MsgRequestLoanResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	var user, found = k.GetUser(ctx, msg.Creator)
+	if !found {
+		user = types.User{
+			Index:               msg.Creator,
+			CreditScore:         0,
+			TimelyPayments:      0,
+			DefaultRate:         strconv.Itoa(0),
+			NumberOfLoans:       0,
+			LoanDuration:        0,
+			NumberOfLoansFunded: 0,
+			LoanFundedDuration:  0,
+			CollateralPercent:   strconv.Itoa(150),
+		}
+		k.SetUser(ctx, user)
+	}
+	var riskLevel = "high"
+	if user.CreditScore > 800 {
+		riskLevel = "low"
+	} else if user.CreditScore > 500 {
+		riskLevel = "medium"
+	} else if user.CreditScore < 500 {
+		riskLevel = "high"
+	}
+
 	var loan = types.Loan{
-		Amoount:    msg.Amount,
-		Fee:        msg.Fee,
-		Collateral: msg.Collateral,
-		Deadline:   msg.Deadline,
-		State:      "requested",
-		Borrower:   msg.Creator,
+		Amoount:       msg.Amount,
+		Fee:           msg.Fee,
+		Collateral:    msg.Collateral,
+		Deadline:      msg.Deadline,
+		State:         "requested",
+		Borrower:      msg.Creator,
+		RiskLevel:     riskLevel,
+		BorrowerScore: strconv.Itoa(int(user.CreditScore)),
 	}
 	borrower, err := sdk.AccAddressFromBech32(msg.Creator)
 	if err != nil {
